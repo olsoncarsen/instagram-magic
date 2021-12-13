e7aab26636089a50ca05f3e071d2955c4a90cf6f
olsoncarsen <gashilovdmitry@yandex.ru>
2021-12-13 22:39:20 +0300

    set confirmation code as argument

--- instagram_magic/instagrambot.py
+++ instagram_magic/instagrambot.py
@@ -12,6 +12,7 @@ import pprint
 
 class InstagramBot:
   def __init__(self, *initial_data, **kwargs):
+    self.use_proxy=False
     for dictionary in initial_data:
       for key in dictionary:
        setattr(self, key, dictionary[key])	
@@ -19,7 +20,8 @@ class InstagramBot:
        setattr(self, key, kwargs[key])
 
     self.session = requests.Session()
-    #self.session.proxies = {'http':'socks5://127.0.0.1:9050', 'https':'socks5://127.0.0.1:9050'}
+    if self.use_proxy:
+      self.session.proxies = {'http':'socks5://127.0.0.1:9050', 'https':'socks5://127.0.0.1:9050'}
     self.session.headers.update({
       'user-agent': 'Mozilla/5.0 (Linux; Android 8.1.0; motorola one Build/OPKS28.63-18-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Instagram 72.0.0.21.98 Android (27/8.1.0; 320dpi; 720x1362; motorola; motorola one; deen_sprout; qcom; pt_BR; 132081645)',
     })
@@ -123,18 +125,20 @@ class InstagramBot:
     print(response.content)
     return response
 
-  def checkConfirmationCode(self):
+  def checkConfirmationCode(self, confirmation_code):
     url = 'https://i.instagram.com/api/v1/accounts/check_confirmation_code/' 
-    confirmation_code = input('Enter your confirmation code: ')
+
     data = {
       'code': confirmation_code,
       'device_id': self.session.cookies.get('mid', domain=".instagram.com"),
       'email': self.email,
     }
+
     response = self.session.post(url, data)
     self.checkClaimHeader(response)
 
     data = response.json()
+    print('CODE DATA ---> ', data);
     print('TOKEN ', self.session.cookies.get('csrftoken', domain=".instagram.com"))
     print('SIGNUP_CODE!!! ', data['signup_code'])
     print(response.content) 
